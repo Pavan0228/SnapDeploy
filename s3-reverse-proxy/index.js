503895743c9027098bb884fab0916708e4997497
Pavan-0228 <pavanrasal4@gmail.com>
2024-12-29 03:24:20 +0530

    refactor s3-reverse-proxy: integrate Prisma for project resolution and update dependencies

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import httpProxy from 'http-proxy';
-import dotenv from 'dotenv';
+const express = require("express");
+const httpProxy = require("http-proxy");
+const dotenv = require("dotenv");
+const prisma = require("../api-server/index.js");
 
+// Load environment variables from .env file
 dotenv.config({
-    path: '.env'
+    path: ".env",
 });
 
 const app = express();
@@ -14,24 +16,33 @@ const BASE_URL = process.env.BASE_URL;
 
 const proxy = httpProxy.createProxy({});
 
-app.use((req, res) => {
+app.use(async (req, res) => {
     const hostName = req.hostname;
-    const subDomain = hostName.split('.')[0];
-    const resolvesTo = `${BASE_URL}${subDomain}`;
+    const subDomain = hostName.split(".")[0];
 
-    return proxy.web(req, res, { target: resolvesTo , changeOrigin: true});
+    const project = await prisma.project.findUnique({
+        where: {
+            subdomain: subDomain,
+        },
+    });
 
+    if (!project) {
+        return res.status(404).send("Project not found");
+    }
+
+    const resolvesTo = BASE_URL + project.id;
+
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
-proxy.on('proxyReq', (proxyReq, req, res) => {
+proxy.on("proxyReq", (proxyReq, req, res) => {
     const url = req.url;
-    if(url === '/') {
-        proxyReq.path += 'index.html';
+    if (url === "/") {
+        proxyReq.path += "index.html";
     }
     return proxyReq;
 });
 
-
-app.listen(port, () => {
-    console.log(`Resverse proxy is running on port ${port}`);
-})
+app.listen(port, function () {
+    console.log("Reverse proxy is running on port " + port);
+});
